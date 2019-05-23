195bcf11a6e35958275d07f0f0c062e75b565134
Benjamin Chiang <chiangbenjamin991@gmail.com>
2019-05-23 08:36:43 -0700

    feat(steamprof): enable api

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,9 +1,11 @@
 import { mergeSchemas } from 'graphql-tools';
 
 import HelloWorld from './HelloWorld/HelloWorld';
+import GetSteamProfile from './GetSteamProfile/GetSteamProfile';
 
 export default mergeSchemas({
     schemas: [
         HelloWorld,
+        GetSteamProfile,
     ],
 });
