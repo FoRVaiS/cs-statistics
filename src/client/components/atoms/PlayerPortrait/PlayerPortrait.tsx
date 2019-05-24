bd1997475481466795476711c5bc27e74c9cd787
Benjamin Chiang <chiangbenjamin991@gmail.com>
2019-05-24 10:59:28 -0700

    feat(portrait): set portrait alt tag

--- src/client/components/atoms/PlayerPortrait/PlayerPortrait.tsx
+++ src/client/components/atoms/PlayerPortrait/PlayerPortrait.tsx
@@ -1,12 +1,14 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
 
 import './PlayerPortrait.styles.scss';
 
 const PlayerPortrait: React.FC<{
     src: string
-}> = ({ src }): React.ReactElement => (
+    playerName: string
+}> = ({ src, playerName }): React.ReactElement => (
     <div className="player-portrait">
-        <img src={src} alt="" className="player-portrait__img" data-testid="portrait" />
+        <img src={src} alt={playerName} className="player-portrait__img" data-testid="portrait" />
     </div>
 );
 
