8d19b51ede632eb53cb7f6584638b5c9f3d8613d
Benjamin Chiang <chiangbenjamin991@gmail.com>
2019-05-29 13:38:30 -0700

    docs(list): populate example with more data

--- src/client/components/atoms/List/List.stories.tsx
+++ src/client/components/atoms/List/List.stories.tsx
@@ -6,18 +6,22 @@ import List from './List';
 
 const data = [
     {
+        ID: 6851,
         Name: 'Henrietta',
         'Miles Ran': 3.2,
     },
     {
+        ID: 3127,
         Name: 'Charlotte',
         'Miles Ran': 2.7,
     },
     {
+        ID: 3128,
         Name: 'Felix',
         'Miles Ran': 0.4,
     },
     {
+        ID: 9702,
         Name: 'Charles',
         'Miles Ran': 2.2,
     },
@@ -27,6 +31,7 @@ storiesOf('List', module)
     .add('displays data under their corresponding columns', (): any => (
         <List
             categories={[
+                'ID',
                 'Name',
                 'Miles Ran',
             ]}
