02d55f62afef8b7b7ca4c3e8ad075d9136c4be9e
Benjamin Chiang <chiangbenjamin991@gmail.com>
2019-05-29 16:19:45 -0700

    feat(api): remove HelloWorld/greet api

--- src/server/api/HelloWorld/HelloWorld.graphql.ts
+++
@@ -1,7 +0,0 @@
-import { gql } from 'apollo-server-express';
-
-export default gql`
-    type Query {
-        greet: String
-    }
-`;
--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,11 +1,9 @@
 import { mergeSchemas } from 'graphql-tools';
 
-import HelloWorld from './HelloWorld/HelloWorld';
 import GetSteamProfile from './GetSteamProfile/GetSteamProfile';
 
 export default mergeSchemas({
     schemas: [
-        HelloWorld,
         GetSteamProfile,
     ],
 });
