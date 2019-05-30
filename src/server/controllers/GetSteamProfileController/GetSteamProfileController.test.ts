32814fb210ae0b35f1de443760ad53a598eed348
Benjamin Chiang <chiangbenjamin991@gmail.com>
2019-05-30 07:40:50 -0700

    test(steamprof): create profile fetch test

--- src/server/controllers/GetSteamProfileController/GetSteamProfileController.test.ts
+++ src/server/controllers/GetSteamProfileController/GetSteamProfileController.test.ts
@@ -13,14 +13,44 @@ beforeEach((): void => {
 
     process.env.STEAMWEB_API_KEY = 'key';
 
-    fakeAxios.get.mockImplementation((): Promise<any> => Promise.resolve({
-        data: {
-            response: {
-                success: 1,
-                steamid: '76561197960434622',
-            },
-        },
-    }));
+    fakeAxios.get.mockImplementation((url: string): Promise<any> => {
+        if (url === 'https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/') {
+            return Promise.resolve({
+                data: {
+                    response: {
+                        success: 1,
+                        steamid: '76561197960434622',
+                    },
+                },
+            });
+        }
+
+        if (url === 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/') {
+            return Promise.resolve({
+                data: {
+                    response: {
+                        players: [
+                            {
+                                avatar: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fd/fdaef0111714471445bb422b8021d3a887c86a99.jpg',
+                                avatarfull: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fd/fdaef0111714471445bb422b8021d3a887c86a99_full.jpg',
+                                avatarmedium: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fd/fdaef0111714471445bb422b8021d3a887c86a99_medium.jpg',
+                                commentpermission: 1,
+                                communityvisibilitystate: 3,
+                                lastlogoff: '1559198657',
+                                personaname: 'al',
+                                personastate: 4,
+                                profilestate: 1,
+                                profileurl: 'https://steamcommunity.com/id/afarnsworth/',
+                                steamid: '76561197960434622',
+                            },
+                        ],
+                    },
+                },
+            });
+        }
+
+        return null;
+    });
 });
 
 describe('The controller', (): void => {
@@ -31,6 +61,17 @@ describe('The controller', (): void => {
             params: {
                 key: process.env.STEAMWEB_API_KEY,
                 vanityurl: 'vanity',
+            },
+        });
+    });
+
+    it('should fetch a player profile', async (): Promise<void> => {
+        await GetSteamProfile('76561197960434622');
+
+        expect(fakeAxios.get).toBeCalledWith('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/', {
+            params: {
+                key: process.env.STEAMWEB_API_KEY,
+                steamids: '76561197960434622',
             },
         });
     });
