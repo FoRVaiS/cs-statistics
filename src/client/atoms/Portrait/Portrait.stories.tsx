662a8b6055ba5cf2c5c2cec3c7fc0090dcd3aacf
Benjamin Chiang <chiangbenjamin991@gmail.com>
2019-05-31 11:57:20 -0700

    docs(portrait): wrap component in a container

--- src/client/atoms/Portrait/Portrait.stories.tsx
+++ src/client/atoms/Portrait/Portrait.stories.tsx
@@ -8,8 +8,16 @@ import Portrait from './Portrait';
 storiesOf('Atoms|Portrait', module)
     .addDecorator(withKnobs)
     .add('renders a picture with text', (): any => (
-        <Portrait
-            src={text('Picture Src', 'https://via.placeholder.com/100x100')}
-            name={text('Text', 'Portrait Text')}
-        />
+        <div style={{
+            margin: '0 auto',
+            marginTop: '2rem',
+            width: '11.5rem',
+            '--portrait__img-p-width': '75%',
+        }}
+        >
+            <Portrait
+                src={text('Picture Src', 'https://via.placeholder.com/100x100')}
+                name={text('Text', 'Portrait Text')}
+            />
+        </div>
     ));
