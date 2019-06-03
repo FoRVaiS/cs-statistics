d4b74dd190269e6e14a3afb7597bef62efcc7feb
Benjamin Chiang <chiangbenjamin991@gmail.com>
2019-06-03 11:12:24 -0700

    refactor(list): remove list section containers

--- src/client/atoms/List/List.tsx
+++ src/client/atoms/List/List.tsx
@@ -7,34 +7,30 @@ const List: React.FC<{
     data: { [key: string]: number | string }[],
 }> = ({ categories, data }): React.ReactElement => (
     <div className="list">
-        <div className="list__head">
-            <div className="list__row list__row--head">
-                {categories.map((category, index): any => <span className="list__category" key={`list-category-${index}`}>{category}</span>)}
-            </div>
+        <div className="list__row list__row--head">
+            {categories.map((category, index): any => <span className="list__category" key={`list-category-${index}`}>{category}</span>)}
         </div>
-        <div className="list__body">
-            {data.map((cellData, rowIndex): any => {
-                const isDataComplete = !categories.map((category): boolean => {
-                    const dataCategories = Object.keys(cellData);
+        {data.map((cellData, rowIndex): any => {
+            const isDataComplete = !categories.map((category): boolean => {
+                const dataCategories = Object.keys(cellData);
 
-                    return dataCategories.includes(category) && cellData[category] !== '';
-                })
-                    .includes(false);
+                return dataCategories.includes(category) && cellData[category] !== '';
+            })
+                .includes(false);
 
-                if (!isDataComplete) {
-                    console.error(`Incomplete data at index ${rowIndex}`);
-                    return null;
-                }
+            if (!isDataComplete) {
+                console.error(`Incomplete data at index ${rowIndex}`);
+                return null;
+            }
 
-                return (
-                    <div className="list__row list__row--body" key={`list-row-${rowIndex}`}>
-                        {categories.map((category, itemIndex): any => (
-                            <span className={`list__item list__item--${category.toLowerCase().replace(/\W+/g, '-')}`} key={`list-row-${rowIndex}-item-${itemIndex}`}>{cellData[category]}</span>
-                        ))}
-                    </div>
-                );
-            })}
-        </div>
+            return (
+                <div className="list__row list__row--body" key={`list-row-${rowIndex}`}>
+                    {categories.map((category, itemIndex): any => (
+                        <span className={`list__item list__item--${category.toLowerCase().replace(/\W+/g, '-')}`} key={`list-row-${rowIndex}-item-${itemIndex}`}>{cellData[category]}</span>
+                    ))}
+                </div>
+            );
+        })}
     </div>
 );
 
