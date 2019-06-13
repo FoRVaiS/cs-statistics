b51c0dd9c6cac0fb37cc129c3f119e6b17e5f6f6
Benjamin Chiang <chiangbenjamin991@gmail.com>
2019-06-13 21:46:23 +0000

    docs(status-page): create status page template storybook

---
+++ src/client/templates/StatusPage/StatusPage.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { withKnobs, number, text } from '@storybook/addon-knobs';
+
+import StatusPageTemplate from './StatusPage';
+
+storiesOf('Templates|Status Page', module)
+    .addDecorator(withKnobs)
+    .add('renders a status page', (): any => (
+        <StatusPageTemplate
+            error={{
+                code: number('Error Code', 404),
+                text: text('Error Code Name', 'Page Not Found'),
+            }}
+        />
+    ))
+    .add('can optionally render a special message', (): any => (
+        <StatusPageTemplate
+            error={{
+                code: number('Error Code', 404),
+                text: text('Error Code Name', 'Page Not Found'),
+            }}
+        >
+            <p>{text('Message', 'The page you were looking for was not found')}</p>
+        </StatusPageTemplate>
+    ));
