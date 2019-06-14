21aa198003328577825bdc5d8bf273b84046366f
Benjamin Chiang <chiangbenjamin991@gmail.com>
2019-06-14 18:53:59 +0000

    docs(ticker): remove unit formatting chapter

--- src/client/atoms/Ticker/Ticker.stories.tsx
+++ src/client/atoms/Ticker/Ticker.stories.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import {
-    withKnobs, text, radios,
-} from '@storybook/addon-knobs';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 import Ticker from './Ticker';
 
@@ -13,14 +11,4 @@ storiesOf('Atoms|Ticker', module)
         <Ticker title={text('Title', 'Title')}>
             {text('Value', '1234657890')}
         </Ticker>
-    ))
-    .add('can display unit symbols next to the value', (): any => {
-        const pSymbol: string = text('Symbol', '$');
-        const pPosition: 'left' | 'right' = radios('Position', { Left: 'left', Right: 'right' }, 'left');
-
-        return (
-            <Ticker title="Value" unit={{ position: pPosition, symbol: pSymbol }}>
-                12
-            </Ticker>
-        );
-    });
+    ));
