bd39938e65571b9ddcf35027cb708b0b24b40187
Benjamin Chiang <chiangbenjamin991@gmail.com>
2019-06-17 21:03:47 +0000

    feat(ticker): create container
    The ticker content must not stretch to a grid cell. Creating a container will help the content
    retain its' original dimensions

--- src/client/atoms/Ticker/Ticker.tsx
+++ src/client/atoms/Ticker/Ticker.tsx
@@ -9,14 +9,18 @@ const Ticker: React.FC<{
         position: 'left' | 'right'
         symbol: string
     }
-}> = ({ title, children }): React.ReactElement => React.cloneElement(children, {
-    className: [
-        'ticker',
-        children.props.className,
-    ]
-        .filter((className): any => typeof className !== 'undefined' && className !== null && className !== '')
-        .join(' '),
-    'data-title': title,
-});
+}> = ({ title, children }): React.ReactElement => (
+    <span className="ticker">
+        {React.cloneElement(children, {
+            className: [
+                'ticker__content',
+                children.props.className,
+            ]
+                .filter((className): any => typeof className !== 'undefined' && className !== null && className !== '')
+                .join(' '),
+            'data-title': title,
+        })}
+    </span>
+);
 
 export default Ticker;
