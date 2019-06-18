0e7f87de7412c2691a2ff0b71f4443c82df35a10
Benjamin Chiang <chiangbenjamin991@gmail.com>
2019-06-18 14:32:55 +0000

    test(ticker): get innerHTML of ticker content

--- src/client/atoms/Ticker/Ticker.test.tsx
+++ src/client/atoms/Ticker/Ticker.test.tsx
@@ -6,22 +6,18 @@ import Ticker from './Ticker';
 
 afterEach(cleanup);
 
-describe('The title', (): any => {
-    it('should populate data-title when ticker is given a title', async (): Promise<any> => {
-        const { container } = render(<Ticker title="MyTitle"><p>0</p></Ticker>);
+it('The title should populate data-title when ticker is given a title', async (): Promise<any> => {
+    const { container } = render(<Ticker title="MyTitle"><p>0</p></Ticker>);
 
-        const ticker = await container.querySelector('.ticker');
+    const ticker = await container.querySelector('.ticker__content');
 
-        expect(ticker!.getAttribute('data-title')).toBe('MyTitle');
-    });
+    expect(ticker!.getAttribute('data-title')).toBe('MyTitle');
 });
 
-describe('The value', (): any => {
-    it('should display when ticker is given a value', async (): Promise<any> => {
-        const { container } = render(<Ticker title=""><p>1234567890</p></Ticker>);
+it('The value should display when ticker is given a value', async (): Promise<any> => {
+    const { container } = render(<Ticker title=""><p>1234567890</p></Ticker>);
 
-        const ticker = await container.querySelector('.ticker');
+    const ticker = await container.querySelector('.ticker__content');
 
-        expect(ticker!.innerHTML).toBe('1234567890');
-    });
+    expect(ticker!.innerHTML).toBe('1234567890');
 });
